0e96848124bf5256b0139fc6233095018ba0d21b
sunao kusu <saradstep@gmail.com>
2018-12-01 19:08:35 +0900

    Initial Commit

---
+++ shopCollection1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shopCollection1
+//
+//  Created by 楠すなを on 2018/12/01.
+//  Copyright © 2018 Yuzu the Cat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
