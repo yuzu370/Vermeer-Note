da84767e95822c280d097075ebe6f819f04cadbf
sunao kusu <saradstep@gmail.com>
2018-12-02 18:22:34 +0900

    first commit

---
+++ shopCollection1/ImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageViewController.swift
+//  shopCollection1
+//
+//  Created by 楠すなを on 2018/12/02.
+//  Copyright © 2018 Yuzu the Cat. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
